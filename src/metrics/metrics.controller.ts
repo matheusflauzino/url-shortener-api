a1e4813e7204df6329b5e0aa20017118d1c7d4e4
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2025-06-22 00:04:10 -0300

    Add Swagger docs and Postman collection

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { MetricsService } from './metrics.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('metrics')
 @Controller('metrics')
 export class MetricsController {
   constructor(private readonly metricsService: MetricsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get Prometheus metrics' })
+  @ApiResponse({ status: 200, description: 'Metrics output' })
   async metrics(@Res() res: Response) {
     res.setHeader('Content-Type', this.metricsService.getContentType());
     res.end(await this.metricsService.getMetrics());
