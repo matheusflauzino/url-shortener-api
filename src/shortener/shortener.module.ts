085f77611f82f3af7ca7dcf1c037347949221811
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2025-06-21 22:58:12 -0300

    feat: configure redis client

--- src/shortener/shortener.module.ts
+++ src/shortener/shortener.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ShortUrl, ShortUrlSchema } from './entities/short-url.schema';
 import { ShortenerController } from './shortener.controller';
 import { ShortenerService } from './shortener.service';
@@ -9,6 +10,22 @@ import { ShortUrlRepository } from './short-url.repository';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: ShortUrl.name, schema: ShortUrlSchema }]),
+    ClientsModule.register([
+      {
+        name: 'SHORTENER_REDIS',
+        transport: Transport.REDIS,
+        options: ((): Record<string, unknown> => {
+          const url = process.env.REDIS_URL ?? 'redis://localhost:6379';
+          const { hostname, port } = new URL(url);
+          return {
+            host: hostname,
+            port: port ? parseInt(port, 10) : 6379,
+            retryAttempts: 3,
+            retryDelay: 1000,
+          };
+        })(),
+      },
+    ]),
   ],
   controllers: [ShortenerController],
   providers: [ShortenerService, ShortCodeService, ShortUrlRepository],
